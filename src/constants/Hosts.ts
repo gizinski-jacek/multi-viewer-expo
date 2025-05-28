e2d023e2baeb5b6e324fcfeb63a008cffecafe30
Jacek Gizinski <jacektrg+github@gmail.com>
2025-05-28 21:04:35 +0200

    File with list of supported video hosts.

---
+++ src/constants/Hosts.ts
@@ -0,0 +1,13 @@
+import { Hosts } from './types';
+
+export const hostList: Hosts[] = [
+	'',
+	'youtube',
+	'youtube-playlist',
+	'twitch',
+	'twitch-vod',
+	'dailymotion',
+	'dailymotion-playlist',
+	'vimeo',
+	'm3u8',
+];
