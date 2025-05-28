70448acb73f560c4f55a7e03b3854bee7a491474
Jacek Gizinski <jacektrg+github@gmail.com>
2025-05-28 20:28:48 +0200

    Removing theme support

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,18 +1,12 @@
-import {
-	DarkTheme,
-	DefaultTheme,
-	ThemeProvider,
-} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 import './global.css';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import React from 'react';
 
 export default function RootLayout() {
-	const colorScheme = useColorScheme();
 	const [loaded] = useFonts({
 		SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
 	});
@@ -23,12 +17,12 @@ export default function RootLayout() {
 	}
 
 	return (
-		<ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+		<>
 			<Stack>
 				<Stack.Screen name='watch' options={{ headerShown: false }} />
 				<Stack.Screen name='+not-found' />
 			</Stack>
 			<StatusBar style='auto' />
-		</ThemeProvider>
+		</>
 	);
 }
